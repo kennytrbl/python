b880e49b236ce734040ae1d744d6d0729d6bfe32
kennytrbl <kennyzhang418@gmail.com>
2024-01-21 01:57:13 -0800

    local push

---
+++ codewars/stop_spinning_my_words.py
@@ -0,0 +1,17 @@
+# stop spinning my words! by kenny zhang
+# https://www.codewars.com/kata/5264d2b162488dc400000001/train/python
+
+def spin_words(sentence):
+    temp = sentence.split()
+    output = []
+    for x in range(len(temp)):
+        if len(temp[x]) >= 5:
+            output.append(temp[x][::-1])
+        else:
+            output.append(temp[x])
+    return " ".join(output)
+
+print(spin_words("Hey fellow warriors"))
+print(spin_words("This is a test"))
+print(spin_words("This is another test"))
+
