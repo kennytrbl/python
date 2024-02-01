e431b319acc37a9af858b76068b5ce743a5bb79e
kennytrbl <kennyzhang418@gmail.com>
2024-01-31 23:59:45 -0800

    fix

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -10,9 +10,9 @@ class Solution(object):
         prevMap = {}
         for i, n in enumerate(nums):
             diff = target - n
-        if diff in prevMap:
-            return [prevMap[diff], i]
-        prevMap[n] = i
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
         return
 
 
